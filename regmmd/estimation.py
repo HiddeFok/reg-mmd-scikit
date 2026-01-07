7d7162a1ef5c014b82f30cb9e1d65f5fa7b65cec
HiddeFok <hidde.fokkema@gmail.com>
2026-01-07 18:36:13 +0100

    Set up the project and started on the Gaussian example

---
+++ regmmd/estimation.py
@@ -0,0 +1,12 @@
+from sklearn.base import BaseEstimator
+
+
+class MMDEstimation(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def predict(self, X):
+        pass
