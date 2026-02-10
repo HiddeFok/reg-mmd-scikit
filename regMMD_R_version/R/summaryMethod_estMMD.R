a410c63b38885bc73e95d1458b7e55349a546d4b
HiddeFok <hidde.fokkema@gmail.com>
2026-02-10 16:17:40 +0100

    removed R version from branch

--- regMMD_R_version/R/summaryMethod_estMMD.R
+++
@@ -1,9 +0,0 @@
-#' Summary method for the \code{class} \code{"estMMD"}
-#' 
-#' @param object An object of \code{class} \code{"estMMD"}.
-#' @param ... Additional arguments (not used).
-#' @return No return value, called only to print information on the output of \code{"estMMD"}.
-#' @export
-summary.estMMD <- function(object, ...) {
-  summary_estMMD(object)
-}
